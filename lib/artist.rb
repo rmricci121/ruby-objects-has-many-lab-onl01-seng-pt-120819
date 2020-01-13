f82242b122cb0423d133382224eccbe21f510df2
Robert M Ricci <rmricci121@gmail.com>
2020-01-13 16:44:23 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class Artist 
+
+  attr_accessor :name 
+    
+  
+  def initialize(name)
+    @name = name 
+    @songs =[]
+  end
+  
+  def add_song(song) 
+    @songs << song 
+    song.artist = self 
+    #binding.pry
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song_by_name(song)
+    song = Song.new(song)
+    add_song(song)
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+  
+  
+end
