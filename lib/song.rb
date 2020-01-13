225675d6fcec63e98a3a69f7f45fb3116f7b6044
Robert M Ricci <rmricci121@gmail.com>
2020-01-13 16:22:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+
+    attr_accessor :name, :artist 
+    
+    @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    #save 
+  end
+  
+  # def save
+  #   @@all << self
+  # end
+  
+  def self.all 
+    @@all
+  end
+  
+  def artist_name
+    if @artist 
+      @artist.name
+    else
+      nil 
+    end
+  end
+
+end
